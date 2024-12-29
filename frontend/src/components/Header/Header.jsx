76a7f7e7c47eef81d4736893984a5e759d142fdc
ALANKRIT AGARWAL <itews2216@glbitm.ac.in>
2024-12-29 20:13:25 +0530

    Navbar and Explore Menu Section Comp

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='header-contents'>
+            <h2>Order your Favourite Food Here</h2>
+            <p>Satisfy your cravings with ease_browse, order, and enjoy delicious meals delivered right to your doorstep anytime, anywhere!</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
