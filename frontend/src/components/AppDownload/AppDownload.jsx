97196b902e56914b003faff5b7604598b118bb24
ALANKRIT AGARWAL <itews2216@glbitm.ac.in>
2025-02-01 17:22:45 +0530

    Production Issues Resolves

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -4,7 +4,7 @@ import { assets } from '../../assets/assets'
 const AppDownload = () => {
   return (
     <div className='app-download' id='app-download'>
-        <p>Download our App for Better Experience <br/> Tomato App </p>
+        <p>Download our App for Better Experience <br/> Nomato App </p>
         <div className='app-download-platforms'>
             <img src={assets.play_store} alt=""/>
             <img src={assets.app_store} alt=""/>
